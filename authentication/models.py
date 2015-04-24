e75cf99bf8cfac73f8fee53fcb84157418870eee
Kix Panganiban <panganiban@outlook.com>
2015-04-24 21:07:22 +0000

    Profile model.

--- authentication/models.py
+++ authentication/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, unique=True)
     picture = models.TextField()
+    type = models.TextField(choices=(('C', 'Commuter'), ('O', 'Operator')))
