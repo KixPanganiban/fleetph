cc531bd90f9eff13d87306a9826f6b03231a9a60
Ryan Wong <ryan.wong022@gmail.com>
2015-04-25 00:37:19 +0800

    added trips module

--- static/javascripts/layout/controllers/index.controller.js
+++ static/javascripts/layout/controllers/index.controller.js
@@ -9,45 +9,16 @@
     .module('myapp.layout.controllers')
     .controller('IndexController', IndexController);
 
-  IndexController.$inject = ['$scope', 'Feed', 'Snackbar', 'Collections', 'Authentication'];
+  IndexController.$inject = ['$scope', 'Feed', 'Snackbar', 'Authentication'];
 
   /**
   * @namespace IndexController
   */
-  function IndexController($scope, Feed, Snackbar, Collections, Authentication) {
+  function IndexController($scope, Feed, Snackbar, Authentication) {
     var vm = this;
 
     vm.isAuthenticated = Authentication.isAuthenticated();
     vm.featured_collections = [];
     vm.posts = new Feed();
-
-    activate();
-
-    /**
-    * @name activate
-    * @desc Actions to be performed when this controller is instantiated
-    * @memberOf myapp.posts.controllers.IndexController
-    */
-    function activate() {
-      Collections.featured().then(collectionsSuccessFn, collectionsErrorFn);
-
-
-      /**
-        * @name collectionsSucessFn
-        * @desc Update `featured_collectionss` on viewmodel
-        */
-      function collectionsSuccessFn(data, status, headers, config) {
-        vm.featured_collections = data.data;
-      }
-
-
-      /**
-        * @name collectionsErrorFn
-        * @desc Show error snackbar
-        */
-      function collectionsErrorFn(data, status, headers, config) {
-        Snackbar.error(data.data.error);
-      }
-    }
   }
 })();
--- static/javascripts/myapp.routes.js
+++ static/javascripts/myapp.routes.js
@@ -13,9 +13,17 @@
   */
   function config($routeProvider) {
     $routeProvider.when('/', {
+      controller: 'IndexController',
+      controllerAs: 'vm',
       templateUrl: '/static/templates/layout/index.html',
     }).when('/trips/new', {
       templateUrl: '/static/templates/trips/new-trip.html',
+    }).when('/requests/new', {
+      templateUrl: '/static/templates/requests/new-request.html',
+    }).when('/trips', {
+      controller: 'TripsController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/trips/trips.html',
     }).otherwise('/');
   }
 })();
